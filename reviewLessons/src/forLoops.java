cce669e9c3c6a9eaf290c50e25367caa50834411
kaancaki7 <kaancaki0@gmail.com>
2025-07-10 18:45:46 +0300

    for loops reminder lesson

---
+++ reviewLessons/src/forLoops.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class forLoops {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //while içindeki koşum doğru olana kadar sonsuz dönebilir fakat for döngülerinde döngümüzü istediğimiz miktarda döndürebiliriz.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for(int i = 1; i <= max; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------");
+
+
+        System.out.print("how many seconds to countdown from: ");
+        int start = scanner.nextInt();
+
+        for(int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("happy new year");
+
+        scanner.close();
+    }
+}
